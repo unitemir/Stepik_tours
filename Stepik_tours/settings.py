ee5f2922cb44394d35075fd8b5ecd46b27007d52
TemirTemi <3010egh@gmail.com>
2021-03-16 12:45:14 +0600

    initial commit

--- Stepik_tours/settings.py
+++ Stepik_tours/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k&6#5(6#0k7z!9b8%rmcszu-^n%bx5$7&4+yv72733ifpt4^ky'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,4 +118,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static'
